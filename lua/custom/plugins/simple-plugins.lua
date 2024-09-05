de96396d99382e424d7bfa4415a8648c7d87a14c
corban <corbanpro@gmail.com>
2024-09-05 14:35:23 -0600

    treesitter context

--- lua/custom/plugins/simple-plugins.lua
+++ lua/custom/plugins/simple-plugins.lua
@@ -1,6 +1,7 @@
 return {
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   'ThePrimeagen/vim-be-good', -- tutorials and games
+  'nvim-treesitter/nvim-treesitter-context',
   { 'Bilal2453/luvit-meta', lazy = true },
   {
     'mbbill/undotree',
