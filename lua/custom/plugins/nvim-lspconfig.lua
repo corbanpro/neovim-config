2250b772786d1304abd9f78db20fbc2d5d8db53c
corban <corbanpro@gmail.com>
2024-09-13 08:28:59 -0600

    add cssls

--- lua/custom/plugins/nvim-lspconfig.lua
+++ lua/custom/plugins/nvim-lspconfig.lua
@@ -58,6 +58,7 @@ return {
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
       local servers = {
+        cssls = {},
         rust_analyzer = {
 
           handlers = {
