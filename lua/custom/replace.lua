509563ba037032ad915c5976dddc55f261d6208d
corban <corbanpro@gmail.com>
2024-09-13 08:29:06 -0600

    update replace functions

--- lua/custom/replace.lua
+++ lua/custom/replace.lua
@@ -1,31 +1,41 @@
 local exports = {}
+local find_replace_string = '\\/:.*$^~[]()'
+
 function exports.n_replace_all()
   local word = vim.fn.expand '<cword>'
-  local escaped_word = vim.fn.escape(word, '\\/.*$^~[]()')
-  vim.cmd(':%s/' .. escaped_word .. '/' .. vim.fn.input('Replace "' .. word .. '" with: ', word) .. '/g')
+  local escaped_word = vim.fn.escape(word, find_replace_string)
+  local replacement = vim.fn.input('Replace "' .. word .. '" with: ', word)
+  local escaped_replacement = vim.fn.escape(replacement, find_replace_string)
+  vim.cmd(':%s/' .. escaped_word .. '/' .. escaped_replacement .. '/g')
   vim.cmd.normal(vim.api.nvim_replace_termcodes('<C-o>', true, true, true))
 end
 
 function exports.v_replace_all()
   vim.cmd 'normal! "zy'
-  local selection = vim.fn.getreg 'z'
-  local escaped_selection = vim.fn.escape(selection, '\\/.*$^~[]()')
-  vim.cmd(':%s/' .. escaped_selection .. '/' .. vim.fn.input('Replace "' .. selection .. '" with: ', selection) .. '/g')
+  local word = vim.fn.getreg 'z'
+  local escaped_word = vim.fn.escape(word, find_replace_string)
+  local replacement = vim.fn.input('Replace "' .. word .. '" with: ', word)
+  local escaped_replacement = vim.fn.escape(replacement, find_replace_string)
+  vim.cmd(':%s/' .. escaped_word .. '/' .. escaped_replacement .. '/g')
   vim.cmd.normal(vim.api.nvim_replace_termcodes('<C-o>', true, true, true))
 end
 
 function exports.n_replace_confirm_all()
   local word = vim.fn.expand '<cword>'
-  local escaped_word = vim.fn.escape(word, '\\/.*$^~[]()')
-  vim.cmd(':%s/' .. escaped_word .. '/' .. vim.fn.input('Replace <' .. word .. '> with: ', word) .. '/gc')
+  local escaped_word = vim.fn.escape(word, find_replace_string)
+  local replacement = vim.fn.input('Replace "' .. word .. '" with: ', word)
+  local escaped_replacement = vim.fn.escape(replacement, find_replace_string)
+  vim.cmd(':%s/' .. escaped_word .. '/' .. escaped_replacement .. '/g')
   vim.cmd.normal(vim.api.nvim_replace_termcodes('<C-o>', true, true, true))
 end
 
 function exports.v_replace_confirm_all()
   vim.cmd 'normal! "zy'
-  local selection = vim.fn.getreg 'z'
-  local escaped_selection = vim.fn.escape(selection, '\\/.*$^~[]()')
-  vim.cmd(':%s/' .. escaped_selection .. '/' .. vim.fn.input('Replace <' .. selection .. '> with: ', selection) .. '/gc')
+  local word = vim.fn.getreg 'z'
+  local escaped_word = vim.fn.escape(word, find_replace_string)
+  local replacement = vim.fn.input('Replace "' .. word .. '" with: ', word)
+  local escaped_replacement = vim.fn.escape(replacement, find_replace_string)
+  vim.cmd(':%s/' .. escaped_word .. '/' .. escaped_replacement .. '/g')
   vim.cmd.normal(vim.api.nvim_replace_termcodes('<C-o>', true, true, true))
 end
 return exports
