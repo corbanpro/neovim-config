1173aad07089672d302543bbfd1511f89067be24
corban <corbanpro@gmail.com>
2024-09-26 16:14:37 -0600

    add multicursor

---
+++ lua/custom/plugins/multicursor.lua
@@ -0,0 +1,10 @@
+return {
+  'smoka7/multicursors.nvim',
+  event = 'VeryLazy',
+  dependencies = {
+    'nvimtools/hydra.nvim',
+  },
+  opts = {},
+  cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
+  keys = require('custom.remap').multicursor,
+}
