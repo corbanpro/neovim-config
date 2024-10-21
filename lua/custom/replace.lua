216d1fdc2cea26f2de0c37ce3dae8ffbd2f13072
corban <corbanpro@gmail.com>
2024-10-21 14:46:16 -0600

    fix replace confirm

--- lua/custom/replace.lua
+++ lua/custom/replace.lua
@@ -38,7 +38,7 @@ function exports.v_replace_confirm_all()
   local escaped_word = vim.fn.escape(word, find_replace_string)
   local replacement = vim.fn.input('Replace "' .. word .. '" with: ', word)
   local escaped_replacement = vim.fn.escape(replacement, find_replace_string)
-  local vim_cmd = ':%sno/' .. escaped_word .. '/' .. escaped_replacement .. '/g'
+  local vim_cmd = ':%sno/' .. escaped_word .. '/' .. escaped_replacement .. '/gc'
   vim.cmd(vim_cmd)
   print(vim_cmd)
 end
