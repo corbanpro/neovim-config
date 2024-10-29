c3521f08b01b59d5c1e1dfd8eca347b660597bcc
corban <corbanpro@gmail.com>
2024-10-29 12:20:54 -0600

    reformat

--- lua/custom/plugins/simple-plugins.lua
+++ lua/custom/plugins/simple-plugins.lua
@@ -1,6 +1,8 @@
 return {
   'tpope/vim-sleuth', -- detect tabstop and shiftwidth automatically
   'theprimeagen/vim-be-good', -- tutorials and games
+  'kkoomen/vim-doge', -- easy documentation
+  'mbbill/undotree', -- undo tree
   {
     'nvim-treesitter/nvim-treesitter-context',
     opts = {
@@ -8,15 +10,5 @@ return {
       multiline_threshold = 1,
     },
   },
-  { 'Bilal2453/luvit-meta', lazy = true },
-  {
-    'mbbill/undotree',
-  },
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-  {
-    'kkoomen/vim-doge', -- easy documentation
-    config = function()
-      -- vim.cmd 'call doge#install()'
-    end,
-  },
 }
