b712bf500fbba88c0cc1fc13d3249ed8a85a3828
corban <corbanpro@gmail.com>
2024-10-29 12:19:50 -0600

    fix bug

--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -2,5 +2,5 @@ return {
   'folke/trouble.nvim',
   opts = {}, -- for default options, refer to the configuration section for custom setup.
   cmd = 'Trouble',
-  keys = require('custom.remap').multicursor,
+  keys = require('custom.remap').trouble,
 }
