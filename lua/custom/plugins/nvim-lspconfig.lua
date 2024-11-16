ced789e0ad7ca094679e5c45aa8ea3539ebae8ee
corban <corbanpro@gmail.com>
2024-11-15 22:36:50 -0700

    add bash-language-server

--- lua/custom/plugins/nvim-lspconfig.lua
+++ lua/custom/plugins/nvim-lspconfig.lua
@@ -82,6 +82,7 @@ return {
       local mason_registry = require 'mason-registry'
       local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server'
       local servers = {
+        bashls = {},
         html = {},
         cssls = {},
         eslint = {},
