8c2c12da651018148182a82f7128b488995a5493
corban <corbanpro@gmail.com>
2024-11-15 22:37:15 -0700

    improve todo comments root dir

--- lua/custom/plugins/todo-comments.lua
+++ lua/custom/plugins/todo-comments.lua
@@ -1,3 +1,6 @@
+local root_patterns = { '.git', '.clang-format', 'pyproject.toml', 'setup.py' }
+local root_dir = vim.fs.dirname(vim.fs.find(root_patterns, { upward = true })[1])
+
 return {
   { 'nvim-lua/plenary.nvim', lazy = true },
   {
@@ -7,7 +10,7 @@ return {
       signs = true,
     },
     keys = {
-      { '<leader>st', '<cmd>:TodoTelescope keywords=TODO,FIX,HACK,BUG,WARNING,WARN<CR>', desc = '[S]earch [T]odo' },
+      { '<leader>st', '<cmd>:TodoTelescope cwd=' .. (root_dir or vim.fn.getcwd()) .. ' keywords=TODO,FIX,HACK,BUG,WARNING,WARN<CR>', desc = '[S]earch [T]odo' },
       {
         ']t',
         function()
