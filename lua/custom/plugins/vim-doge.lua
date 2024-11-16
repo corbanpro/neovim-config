57f9af2fe01aa0fde3ae23d17143924e34bac86f
corban <corbanpro@gmail.com>
2024-11-15 22:37:21 -0700

    fix vim doge load

--- lua/custom/plugins/vim-doge.lua
+++ lua/custom/plugins/vim-doge.lua
@@ -3,11 +3,5 @@ vim.g.doge_mapping = '<leader>od'
 return {
   {
     'kkoomen/vim-doge',
-    keys = {
-      {
-        '<leader>od',
-        desc = '[O]pen [D]ocumentation',
-      },
-    },
   },
 }
