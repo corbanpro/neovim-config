966c075855f7529d18e2bbc03f638b3e991f1299
corban <corbanpro@gmail.com>
2025-01-25 16:47:23 -0700

    add quotes to uuid

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -17,10 +17,11 @@ vim.keymap.set('n', 'J', 'mzJ`z', { desc = "join lines, don't move cursor" })
 local function generate_uuid()
   local random = math.random
   local template = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
-  return string.gsub(template, '[xy]', function(c)
+  local uuid = string.gsub(template, '[xy]', function(c)
     local v = (c == 'x') and random(0, 15) or random(8, 11)
     return string.format('%x', v)
   end)
+  return '"' .. uuid .. '"'
 end
 
 -- Insert UUID at the cursor location
