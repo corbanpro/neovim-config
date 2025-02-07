2d1fd8dd7a663b3561a0a8dcaf4a611075fb1ec9
corban <corbanpro@gmail.com>
2025-02-07 15:10:38 -0700

    add icon picker shortcut

--- lua/custom/plugins/icon-picker.lua
+++ lua/custom/plugins/icon-picker.lua
@@ -1,6 +1,13 @@
 return {
   'ziontee113/icon-picker.nvim',
   cmd = { 'IconPickerNormal', 'IconPickerYank', 'IconPickerInsert' },
+  keys = {
+    {
+      '<leader>ii',
+      ':IconPickerNormal<CR>',
+      desc = '[I]nsert [I]con',
+    },
+  },
   opts = {
     -- disable_legacy_commands = true,
   },
