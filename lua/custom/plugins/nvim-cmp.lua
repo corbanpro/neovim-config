8e394c3475265927213d944d43dd4a2c8389719f
corban <corbanpro@gmail.com>
2025-03-20 11:23:51 -0600

    add go snippets

--- lua/custom/plugins/nvim-cmp.lua
+++ lua/custom/plugins/nvim-cmp.lua
@@ -34,6 +34,7 @@ return {
     event = 'InsertEnter',
     config = function()
       require('luasnip.loaders.from_vscode').lazy_load()
+      require('luasnip.loaders.from_snipmate').lazy_load()
     end,
   },
   {
