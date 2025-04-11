37ebd8f5fcd79ec5e5dbc871081a778a8cc1e13f
corban <corbanpro@gmail.com>
2025-04-11 11:56:02 -0600

    get rid of deprecated null-ls

--- lua/custom/plugins/null-ls.lua
+++ lua/custom/plugins/null-ls.lua
@@ -1,9 +1,9 @@
 return {
-  {
-    'jose-elias-alvarez/null-ls.nvim',
-    ft = 'lua',
-    config = function()
-      require 'null-ls'
-    end,
-  },
+  -- {
+  --   'jose-elias-alvarez/null-ls.nvim',
+  --   ft = 'lua',
+  --   config = function()
+  --     require 'null-ls'
+  --   end,
+  -- },
 }
