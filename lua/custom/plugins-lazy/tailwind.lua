5499df8bee1d6ac63a56b16e0c93b2129b1d955c
Corban Procuniar <corban.procuniar@getsignals.ai>
2025-11-19 14:19:10 -0700

    use local version of tailwind-tools

--- lua/custom/plugins-lazy/tailwind.lua
+++ lua/custom/plugins-lazy/tailwind.lua
@@ -1,11 +1,10 @@
 return {
-  'luckasRanarison/tailwind-tools.nvim',
+  'corbanpro/tailwind-tools.nvim',
   name = 'tailwind-tools',
   build = ':UpdateRemotePlugins',
   dependencies = {
     'nvim-treesitter/nvim-treesitter',
     'nvim-telescope/telescope.nvim', -- optional
-    'neovim/nvim-lspconfig', -- optional
   },
   ft = { 'html', 'css', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'vue', 'templ' },
   opts = {}, -- your configuration
