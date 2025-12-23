e636ae78523d7873193939146ceaacb26273ffba
Corban Procuniar <corban.procuniar@getsignals.ai>
2025-12-23 15:19:46 -0700

    replace markdown preview with vivify

---
+++ lua/custom/plugins-lazy/vivify.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'jannis-baum/vivify.vim',
+    keys = {
+      {
+        '<leader>op',
+        '<cmd>:Vivify<CR>',
+        desc = '[O]pen Markdown [P]review',
+      },
+    },
+  },
+}
