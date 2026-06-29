0dd1b769722dc084fd04483b4a8625bc856e3b23
Corban Procuniar <corban.procuniar@getsignals.ai>
2026-06-28 22:13:17 -0600

    simplify auto pairs

--- lua/custom/plugins-lazy/autopairs.lua
+++ lua/custom/plugins-lazy/autopairs.lua
@@ -5,15 +5,6 @@ return {
   {
     'windwp/nvim-autopairs',
     event = 'InsertEnter',
-    config = function()
-      require('nvim-autopairs').setup {}
-      local Rule = require 'nvim-autopairs.rule'
-      local npairs = require 'nvim-autopairs'
-      npairs.add_rule(Rule('/** ', ' */', { 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'commonjs', 'modulejs', 'vue' }))
-      -- If you want to automatically add `(` after selecting a function or method
-      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-      local cmp = require 'cmp'
-      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-    end,
+    config = true,
   },
 }
